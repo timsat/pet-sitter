422b2c4c0475c7e02927dce310f02a3aaf024ddd
Timur Sattarov <tim.helloworld@gmail.com>
2020-01-12 23:43:26 +0400

    Experimenting with custom CNN

---
+++ mynet.py
@@ -0,0 +1,23 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import tensorflow as tf
+
+from tensorflow.keras import datasets, layers, models
+import matplotlib.pyplot as plt
+
+model = models.Sequential()
+model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(300, 300, 3)))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+
+model.add(layers.Flatten())
+model.add(layers.Dense(64, activation='relu'))
+model.add(layers.Dense(2, activation='sigmoid'))
+model.summary()
+model.compile(optimizer='adam',
+              loss='mse', metrics=['accuracy'])
+
+model.fit()
+
